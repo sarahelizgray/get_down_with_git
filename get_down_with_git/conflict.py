b66c6a528bea4533dd6f70acbf1f2065acd9b755
Sarah Gray <sarah.eliz.gray@gmail.com>
2017-11-21 09:40:01 -0500

    fix typo

--- get_down_with_git/conflict.py
+++ get_down_with_git/conflict.py
@@ -1,3 +1,3 @@
-# Hey hey, I wrote this and commited it to master
+# Hey hey, I wrote this and committed it to master
 # after the see_merge_conflict branch was cut.
 # This should cause a merge conflict.
